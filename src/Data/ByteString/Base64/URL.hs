1d9243864ebf3a2d03b924b6f3d2d829b8af022f
Emily Pillmore <emily@kadena.io>
2019-11-06 19:35:41 -0500

    api

--- src/Data/ByteString/Base64/URL.hs
+++ src/Data/ByteString/Base64/URL.hs
@@ -1,1 +1,30 @@
-module Data.ByteString.Base64.URL where
+module Data.ByteString.Base64.URL
+( encodeBase64
+, decodeBase64
+, decodeBase64Lenient
+, encodeBase64Unpadded
+, decodeBase64Unpadded
+, decodeBase64UnpaddedLenient
+) where
+
+import Data.ByteString (ByteString)
+import Data.Text (Text)
+
+
+encodeBase64 :: ByteString -> ByteString
+encodeBase64 = undefined
+
+decodeBase64 :: ByteString -> Either Text ByteString
+decodeBase64 = undefined
+
+decodeBase64Lenient :: ByteString -> ByteString
+decodeBase64Lenient = undefined
+
+encodeBase64Unpadded :: ByteString -> ByteString
+encodeBase64Unpadded = undefined
+
+decodeBase64Unpadded :: ByteString -> Either Text ByteString
+decodeBase64Unpadded = undefined
+
+decodeBase64UnpaddedLenient :: ByteString -> ByteString
+decodeBase64UnpaddedLenient = undefined
--- src/Data/Text/Encoding/Base64.hs
+++ src/Data/Text/Encoding/Base64.hs
@@ -1,1 +1,42 @@
-module Data.Text.Encoding.Base64 where
+module Data.Text.Encoding.Base64
+( encodeBase64Text
+, decodeBase64Text
+, decodeBase64TextLenient
+, encodeBase64TextUnpadded
+, decodeBase64TextUnpadded
+, decodeBase64TextUnpaddedLenient
+) where
+
+
+import Data.ByteString.Base64
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+
+
+encodeBase64Text :: Text -> Text
+encodeBase64Text = T.decodeUtf8 . encodeBase64 . T.encodeUtf8
+
+decodeBase64Text :: Text -> Either Text Text
+decodeBase64Text = fmap T.decodeUtf8 . decodeBase64 . T.encodeUtf8
+
+decodeBase64TextLenient :: Text -> Text
+decodeBase64TextLenient = T.decodeUtf8
+    . decodeBase64Lenient
+    . T.encodeUtf8
+
+encodeBase64TextUnpadded :: Text -> Text
+encodeBase64TextUnpadded = T.decodeUtf8
+    . encodeBase64Unpadded
+    . T.encodeUtf8
+
+decodeBase64TextUnpadded :: Text -> Either Text Text
+decodeBase64TextUnpadded = fmap T.decodeUtf8
+    . decodeBase64Unpadded
+    . T.encodeUtf8
+
+decodeBase64TextUnpaddedLenient :: Text -> Text
+decodeBase64TextUnpaddedLenient = T.decodeUtf8
+    . decodeBase64UnpaddedLenient
+    . T.encodeUtf8
