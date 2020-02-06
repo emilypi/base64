566d5e36a0460de7f0c9c95e24bf21ca18b13219
Emily Pillmore <emily@kadena.io>
2020-02-06 15:42:30 -0500

    remove output, tables.h

--- cbits/tables.h
+++
@@ -1,4 +0,0 @@
-static const uint16_t base64_table_enc_12bit_le_std[];
-static const uint16_t base64_table_enc_12bit_be_std[];
-static const uint16_t base64_table_enc_12bit_le_url[];
-static const uint16_t base64_table_enc_12bit_be_url[];
