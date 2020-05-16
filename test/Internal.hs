de5b0e978467257bcb24b8672e8a9d46031a33ae
Emily Pillmore <emilypi@cohomolo.gy>
2020-05-16 19:39:14 -0400

    add property testing harness

---
+++ test/Internal.hs
@@ -0,0 +1,189 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+-- |
+-- Module       : Main
+-- Copyright    : (c) 2019-2020 Emily Pillmore
+-- License      : BSD-style
+--
+-- Maintainer   : Emily Pillmore <emilypi@cohomolo.gy>
+-- Stability    : Experimental
+-- Portability  : portable
+--
+-- This module contains internal test harnesses for `base64`
+--
+module Internal where
+
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Short as SBS
+import "base64" Data.ByteString.Base64 as B64
+import "base64" Data.ByteString.Base64.URL as B64U
+import "base64" Data.ByteString.Lazy.Base64 as LB64
+import "base64" Data.ByteString.Lazy.Base64.URL as LB64U
+import "base64" Data.ByteString.Short.Base64 as SB64
+import "base64" Data.ByteString.Short.Base64.URL as SB64U
+import "base64" Data.Text.Encoding.Base64 as T64
+import "base64" Data.Text.Encoding.Base64.URL as T64U
+import "base64" Data.Text.Lazy.Encoding.Base64 as TL64
+import "base64" Data.Text.Lazy.Encoding.Base64.URL as TL64U
+import "base64" Data.Text.Short.Encoding.Base64 as TS64
+import "base64" Data.Text.Short.Encoding.Base64.URL as TS64U
+
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Short as TS
+
+import Test.QuickCheck.Instances
+import Test.QuickCheck hiding (label)
+
+
+-- ------------------------------------------------------------------ --
+-- Quickcheck instances
+
+instance Arbitrary TS.ShortText where
+  arbitrary = TS.fromText <$> arbitrary
+  shrink xs = fmap TS.fromText $ shrink (TS.toText xs)
+
+instance CoArbitrary TS.ShortText where
+  coarbitrary = coarbitrary . TS.toText
+
+instance Function TS.ShortText where
+  function = functionMap
+    (T.unpack . TS.toText)
+    (TS.fromText . T.pack)
+
+-- ------------------------------------------------------------------ --
+-- Test Harnesses
+
+data B64 = B64
+data LB64 = LB64
+data SB64 = SB64
+data T64 = T64
+data TL64 = TL64
+data TS64 = TS64
+
+-- | This class provides the generic API definition for
+-- the base64 std alphabet
+
+class
+  ( Eq bs, Show bs
+  , Arbitrary bs
+  , CoArbitrary bs
+  ) => Harness a bs | a -> bs
+  where
+
+  label :: String
+  encode :: bs -> bs
+  encodeUrl :: bs -> bs
+  encodeUrlNopad :: bs -> bs
+
+  decode :: bs -> Either Text bs
+  decodeUrl :: bs -> Either Text bs
+  decodeUrlPad :: bs -> Either Text bs
+  decodeUrlNopad :: bs -> Either Text bs
+
+  lenientUrl :: bs -> bs
+  lenient :: bs -> bs
+
+  validate :: bs -> Bool
+  validateUrl :: bs -> Bool
+
+
+instance Harness B64 BS.ByteString where
+  label = "ByteString"
+
+  encode = B64.encodeBase64'
+  decode = B64.decodeBase64
+  lenient = B64.decodeBase64Lenient
+  validate = B64.isBase64
+  encodeUrl = B64U.encodeBase64'
+  encodeUrlNopad = B64U.encodeBase64Unpadded'
+  decodeUrl = B64U.decodeBase64
+  decodeUrlPad = B64U.decodeBase64Padded
+  decodeUrlNopad = B64U.decodeBase64Unpadded
+  lenientUrl = B64U.decodeBase64Lenient
+  validateUrl = B64U.isBase64Url
+
+instance Harness LB64 LBS.ByteString where
+  label = "Lazy ByteString"
+
+  encode = LB64.encodeBase64'
+  decode = LB64.decodeBase64
+  lenient = LB64.decodeBase64Lenient
+  validate = LB64.isBase64
+  encodeUrl = LB64U.encodeBase64'
+  encodeUrlNopad = LB64U.encodeBase64Unpadded'
+  decodeUrl = LB64U.decodeBase64
+  decodeUrlPad = LB64U.decodeBase64Padded
+  decodeUrlNopad = LB64U.decodeBase64Unpadded
+  lenientUrl = LB64U.decodeBase64Lenient
+  validateUrl = LB64U.isBase64Url
+
+instance Harness SB64 SBS.ShortByteString where
+  label = "Short ByteString"
+
+  encode = SB64.encodeBase64'
+  decode = SB64.decodeBase64
+  lenient = SB64.decodeBase64Lenient
+  validate = SB64.isBase64
+  encodeUrl = SB64U.encodeBase64'
+  encodeUrlNopad = SB64U.encodeBase64Unpadded'
+  decodeUrl = SB64U.decodeBase64
+  decodeUrlPad = SB64U.decodeBase64Padded
+  decodeUrlNopad = SB64U.decodeBase64Unpadded
+  lenientUrl = SB64U.decodeBase64Lenient
+  validateUrl = SB64U.isBase64Url
+
+instance Harness T64 Text where
+  label = "Text"
+
+  encode = T64.encodeBase64
+  decode = T64.decodeBase64
+  lenient = T64.decodeBase64Lenient
+  validate = T64.isBase64
+  encodeUrl = T64U.encodeBase64
+  encodeUrlNopad = T64U.encodeBase64Unpadded
+  decodeUrl = T64U.decodeBase64
+  decodeUrlPad = T64U.decodeBase64Padded
+  decodeUrlNopad = T64U.decodeBase64Unpadded
+  lenientUrl = T64U.decodeBase64Lenient
+  validateUrl = T64U.isBase64Url
+
+instance Harness TL64 TL.Text where
+  label = "Lazy Text"
+
+  encode = TL64.encodeBase64
+  decode = TL64.decodeBase64
+  lenient = TL64.decodeBase64Lenient
+  validate = TL64.isBase64
+  encodeUrl = TL64U.encodeBase64
+  encodeUrlNopad = TL64U.encodeBase64Unpadded
+  decodeUrl = TL64U.decodeBase64
+  decodeUrlPad = TL64U.decodeBase64Padded
+  decodeUrlNopad = TL64U.decodeBase64Unpadded
+  lenientUrl = TL64U.decodeBase64Lenient
+  validateUrl = TL64U.isBase64Url
+
+instance Harness TS64 TS.ShortText where
+  label = "Short Text"
+
+  encode = TS64.encodeBase64
+  decode = TS64.decodeBase64
+  lenient = TS64.decodeBase64Lenient
+  validate = TS64.isBase64
+  encodeUrl = TS64U.encodeBase64
+  encodeUrlNopad = TS64U.encodeBase64Unpadded
+  decodeUrl = TS64U.decodeBase64
+  decodeUrlPad = TS64U.decodeBase64Padded
+  decodeUrlNopad = TS64U.decodeBase64Unpadded
+  lenientUrl = TS64U.decodeBase64Lenient
+  validateUrl = TS64U.isBase64Url
---
+++ test/Main.hs
@@ -0,0 +1,208 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+-- |
+-- Module       : Main
+-- Copyright    : (c) 2019-2020 Emily Pillmore
+-- License      : BSD-style
+--
+-- Maintainer   : Emily Pillmore <emilypi@cohomolo.gy>
+-- Stability    : Experimental
+-- Portability  : portable
+--
+-- This module contains the test implementation for the `base64` package
+--
+module Main
+( main
+, tests
+) where
+
+
+import qualified Data.ByteString as BS
+import "base64" Data.ByteString.Base64 as B64
+import "base64" Data.ByteString.Base64.URL as B64U
+import qualified "base64-bytestring" Data.ByteString.Base64 as Bos
+import qualified "base64-bytestring" Data.ByteString.Base64.URL as BosU
+
+import Internal
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck (testProperty)
+
+
+main :: IO ()
+main = defaultMain tests
+
+
+tests :: TestTree
+tests = testGroup "Base64 Tests"
+  [ mkTree B64
+  , mkTree LB64
+  , mkTree SB64
+  , mkTree T64
+  , mkTree TL64
+  , mkTree TS64
+  ]
+
+mkTree :: forall a b. Harness a b => a -> TestTree
+mkTree a = testGroup (label @a)
+  [ properties a
+  , rfcVectors
+  , paddingTests
+  ]
+
+-- ---------------------------------------------------------------- --
+-- Property tests
+
+properties :: forall a b. Harness a b => a -> TestTree
+properties a = testGroup "Property tests"
+  [ prop_roundtrip a
+  , prop_correctness a
+  , prop_url_padding a
+  , prop_bos_coherence
+  ]
+
+prop_roundtrip :: forall a b. Harness a b => a -> TestTree
+prop_roundtrip _ = testGroup "prop_roundtrip"
+  [ testProperty "prop_std_roundtrip" $ \bs ->
+      Right bs == decode @a (encode @a bs)
+  , testProperty "prop_url_roundtrip" $ \bs ->
+      Right bs == decodeUrl @a (encodeUrl @a bs)
+  , testProperty "prop_url_roundtrip_nopad" $ \bs ->
+      Right bs == decodeUrl @a (encodeUrlNopad @a bs)
+  ]
+
+prop_correctness :: forall a b. Harness a b => a -> TestTree
+prop_correctness _ = testGroup "prop_validity"
+  [ testProperty "prop_std_valid" $ \bs ->
+    validate @a bs
+  , testProperty "prop_url_valid" $ \bs ->
+    validateUrl @a bs
+  ]
+
+prop_url_padding :: forall a b. Harness a b => a -> TestTree
+prop_url_padding _ = testGroup "prop_url_padding"
+  [ testProperty "prop_url_nopad_roundtrip" $ \bs ->
+      Right bs == decodeUrlNopad @a (encodeUrlNopad @a bs)
+  , testProperty "prop_url_pad_roundtrip" $ \bs ->
+      Right bs == decodeUrlPad @a (encodeUrl @a bs)
+  , testProperty "prop_url_decode_invariant" $ \bs ->
+      (Right bs == decodeUrl @a (encodeUrl @a bs)
+      || Right bs == decodeUrlNopad @a (encodeUrlNopad @a bs))
+      || Right bs == decodeUrlPad @a (encodeUrl @a bs)
+  , testProperty "prop_url_padding_coherence" $ \bs ->
+      Right bs == decodeUrl @a (encodeUrl @a bs)
+      && Right bs == decodeUrlPad @a (encodeUrl @a bs)
+  ]
+
+
+prop_bos_coherence :: TestTree
+prop_bos_coherence = testGroup "prop_bos_coherence"
+  [ testProperty "prop_std_bos_coherence" $ \bs ->
+      Right bs == B64.decodeBase64 (B64.encodeBase64' bs)
+      && Right bs == Bos.decode (Bos.encode bs)
+  , testProperty "prop_url_bos_coherence" $ \bs ->
+      Right bs == B64U.decodeBase64 (B64U.encodeBase64' bs)
+      && Right bs == BosU.decode (BosU.encode bs)
+  ]
+
+-- ---------------------------------------------------------------- --
+-- Unit tests
+
+rfcVectors :: TestTree
+rfcVectors = testGroup "RFC 4648 Test Vectors"
+    [ testGroup "std alphabet"
+      [ testCaseStd "" ""
+      , testCaseStd "f" "Zg=="
+      , testCaseStd "f" "Zg=="
+      , testCaseStd "fo" "Zm8="
+      , testCaseStd "foo" "Zm9v"
+      , testCaseStd "foob" "Zm9vYg=="
+      , testCaseStd "fooba" "Zm9vYmE="
+      , testCaseStd "foobar" "Zm9vYmFy"
+      ]
+    , testGroup "url-safe alphabet"
+      [ testCaseUrl "" ""
+      , testCaseUrl "<" "PA=="
+      , testCaseUrl "<<" "PDw="
+      , testCaseUrl "<<?" "PDw_"
+      , testCaseUrl "<<??" "PDw_Pw=="
+      , testCaseUrl "<<??>" "PDw_Pz4="
+      , testCaseUrl "<<??>>" "PDw_Pz4-"
+      ]
+    ]
+  where
+    testCaseStd :: BS.ByteString -> BS.ByteString -> TestTree
+    testCaseStd s t = testCase (show $ if s == "" then "empty" else s) $ do
+      t @=? B64.encodeBase64' s
+      Right s @=? B64.decodeBase64 (B64.encodeBase64' s)
+
+    testCaseUrl :: BS.ByteString -> BS.ByteString -> TestTree
+    testCaseUrl s t = testCase (show $ if s == "" then "empty" else s) $ do
+      t @=? B64U.encodeBase64' s
+      Right s @=? B64U.decodeBase64Padded t
+
+paddingTests :: TestTree
+paddingTests = testGroup "Padding tests"
+    [ testGroup "URL decodePadding coherence"
+      [ ptest "<" "PA=="
+      , ptest "<<" "PDw="
+      , ptest "<<?" "PDw_"
+      , ptest "<<??" "PDw_Pw=="
+      , ptest "<<??>" "PDw_Pz4="
+      , ptest "<<??>>" "PDw_Pz4-"
+      ]
+    , testGroup "URL decodeUnpadded coherence"
+      [ utest "<" "PA"
+      , utest "<<" "PDw"
+      , utest "<<?" "PDw_"
+      , utest "<<??" "PDw_Pw"
+      , utest "<<??>" "PDw_Pz4"
+      , utest "<<??>>" "PDw_Pz4-"
+      ]
+    ]
+  where
+    ptest :: BS.ByteString -> BS.ByteString -> TestTree
+    ptest s t =
+      testCaseSteps (show $ if t == "" then "empty" else t) $ \step -> do
+        let u = B64U.decodeBase64Unpadded t
+            v = B64U.decodeBase64Padded t
+
+        if BS.last t == 0x3d then do
+          step "Padding required: no padding fails"
+          u @=? Left "Base64-encoded bytestring has invalid padding"
+
+          step "Padding required: padding succeeds"
+          v @=? Right s
+        else do
+          step "String has no padding: decodes should coincide"
+          u @=? Right s
+          v @=? Right s
+          v @=? u
+
+    utest :: BS.ByteString -> BS.ByteString -> TestTree
+    utest s t =
+      testCaseSteps (show $ if t == "" then "empty" else t) $ \step -> do
+        let u = B64U.decodeBase64Padded t
+            v = B64U.decodeBase64Unpadded t
+
+        if BS.length t `mod` 4 == 0 then do
+          step "String has no padding: decodes should coincide"
+          u @=? Right s
+          v @=? Right s
+          v @=? u
+        else do
+          step "Unpadded required: padding fails"
+          u @=? Left "Base64-encoded bytestring requires padding"
+
+          step "Unpadded required: unpadding succeeds"
+          v @=? Right s
