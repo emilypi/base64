5ebb73a18a3e0fa321df168e2cf2afcc51ee7f47
Emily Pillmore <emilypi@cohomolo.gy>
2020-07-23 11:34:13 -0400

    update nix

--- default.nix
+++ default.nix
@@ -5,20 +5,22 @@ let
   inherit (nixpkgs) pkgs;
 
   f = { mkDerivation, base, base64-bytestring, bytestring
-      , criterion, deepseq, memory, random-bytestring, stdenv, tasty
-      , tasty-hunit, text
+      , criterion, deepseq, ghc-byteorder, QuickCheck, random-bytestring
+      , stdenv, tasty, tasty-hunit, tasty-quickcheck, text, text-short
       }:
       mkDerivation {
         pname = "base64";
-        version = "0.4.1";
+        version = "0.4.2.2";
         src = ./.;
-        libraryHaskellDepends = [ base bytestring text ];
+        libraryHaskellDepends = [
+          base bytestring deepseq ghc-byteorder text text-short
+        ];
         testHaskellDepends = [
-          base base64-bytestring bytestring random-bytestring tasty
-          tasty-hunit text
+          base base64-bytestring bytestring QuickCheck random-bytestring
+          tasty tasty-hunit tasty-quickcheck text text-short
         ];
         benchmarkHaskellDepends = [
-          base base64-bytestring bytestring criterion deepseq memory
+          base base64-bytestring bytestring criterion deepseq
           random-bytestring text
         ];
         homepage = "https://github.com/emilypi/base64";
