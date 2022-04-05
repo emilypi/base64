7c19937b20a5016476724564850fb78c7147cae5
Emily Pillmore <emilypi@cohomolo.gy>
2022-04-05 11:12:51 -0600

    add commentary

--- src/Data/Base64/Types.hs
+++ src/Data/Base64/Types.hs
@@ -26,6 +26,7 @@ module Data.Base64.Types
 , coerceBase64
 , type UrlAlphabet
 , type StdAlphabet
+, type NonStandardAlphabet
 ) where
 
 
@@ -38,6 +39,14 @@ import GHC.TypeLits
 
 -- | Assert a value to be encoded in a specific way
 --
+-- /Warning/: This is a blind assertion that a particular
+-- value is base64 encoded in some alphabet. If you are not
+-- sure of the provenance of the value, you may experience
+-- odd behavior when attempting to decode. Use at your own
+-- risk. If I see any issues logged on this project from
+-- negligent use of this or 'coerceBase64', Sofia and I will
+-- smite you.
+--
 assertBase64 :: forall k a. a -> Base64 k a
 assertBase64 = Base64
 
@@ -47,6 +56,14 @@ extractBase64 :: forall a k. Base64 k a -> a
 extractBase64 (Base64 a) = a
 
 -- | Coerce the alphabet of a base64-encoded bytestring
+--
+-- /Warning/: This is a blind assertion that a particular
+-- value is base64 encoded in some alphabet. If you are not
+-- sure of the provenance of the value, you may experience
+-- odd behavior when attempting to decode. Use at your own
+-- risk. If I see any issues logged on this project from
+-- negligent use of this or 'assertBase64', Sofia and I will
+-- smite you.
 --
 coerceBase64 :: forall j k a. Base64 k a -> Base64 j a
 coerceBase64 = coerce
@@ -75,4 +92,12 @@ type family StdAlphabet k :: Constraint where
     ( 'Text "Cannot prove base64 value is encoded using the std \
             \alphabet. Please re-encode using the std encoders, or use \
             \a lenient decoder for the std alphabet instead."
+    )
+
+type family NonStandardAlphabet k :: Constraint where
+  NonStandardAlphabet 'NonStandard = ()
+  NonStandardAlphabet _ = TypeError
+    ( 'Text "Cannot prove base64 value is encoded using a non-standard \
+            \alphabet. Please re-encode and assert/coerce the alphabet, \
+            \and use a lenient decoder."
     )
